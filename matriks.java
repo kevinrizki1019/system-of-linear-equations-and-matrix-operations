9ee9804c3cbbdbab8d01e2fe81afd0c69e7cdf4b
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-09-23 19:09:23 +0700

    merge

--- matriks.java
+++ matriks.java
@@ -10,13 +10,8 @@ BASIC:
 import java.io.*;
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class matriks {
-=======
-
 public class matriks 
 {
->>>>>>> dba88f13f2a80ad0f68c23cbc178b8b7c776b3ac
     Scanner input = new Scanner(System.in);
 
     /*** PROPERTIES ***/
@@ -174,7 +169,6 @@ public class matriks
         // F.s. temp berisi matriks cofactor p,q dari this.Mat 
         int i = 0, j = 0;
 
-<<<<<<< HEAD
         double[][] matOut = new double[n-1][n-1]; 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
@@ -184,20 +178,6 @@ public class matriks
                     matOut[i][j++] = matIn[row][col];
 
                     if (j == (n - 1)) {
-=======
-        for (int row = 0; row < n; row++) 
-        {
-            for (int col = 0; col < n; col++) 
-            {
-
-                // Jika elemet berindeks [p,q] maka akan dilewati
-                if (row != p && col != q) 
-                {
-                    temp[i][j++] = this.Mat[row][col];
-
-                    if (j == n - 1) 
-                    {
->>>>>>> dba88f13f2a80ad0f68c23cbc178b8b7c776b3ac
                         j = 0;
                         i++;
                     }
@@ -251,7 +231,6 @@ public class matriks
         }
     }
 
-<<<<<<< HEAD
     public double[][] getMatriksCofactor(double matIn[][], int n) {
         // !! Masih salah
         // Coba 2 X 2:
@@ -271,17 +250,6 @@ public class matriks
                 } else {
                     matOut[i][j] = -1 * determinantOfMatrix(temp, n - 1);    
                 }
-=======
-    public void getMatriksCofactor(double result[][]) 
-    {
-        double temp[][] = result;
-        for (int i = 0; i < getidxBaris(); i++) 
-        {
-            for (int j = 0; j < getidxKolom(); j++) 
-            {
-                this.getCofactor(temp, i, j, getidxBaris());
-                result[i][j] = determinantOfMatrix(temp, getidxBaris() - 1);
->>>>>>> dba88f13f2a80ad0f68c23cbc178b8b7c776b3ac
             }
         }
         return matOut;
@@ -298,17 +266,9 @@ public class matriks
         }
     }
 
-<<<<<<< HEAD
     public void getAdjoin(double result[][]) {
         matriks cofactor = new matriks();
         cofactor.Mat = getMatriksCofactor(this.Mat, idxBaris);
-=======
-    public void getAdjoin(double result[][]) 
-    {
-        matriks cofactor = new matriks(this.idxBaris, this.idxBaris);
-        cofactor = this;
-        this.getMatriksCofactor(cofactor.Mat);
->>>>>>> dba88f13f2a80ad0f68c23cbc178b8b7c776b3ac
         getTranspose(cofactor.Mat);
         result = cofactor.Mat;
     }
@@ -419,96 +379,6 @@ public class matriks
             }
         }
     }
-<<<<<<< HEAD
-        
-=======
-        public void BacaMatriks() 
-        {
-            for(int i=0; i<this.idxBaris; i++)
-            {
-                for(int j=0; j<this.idxKolom; j++)
-                {
-                    this.Mat[i][j] = input.nextDouble();
-                }
-            }    
-        } 
-    
-        public void BacaMatriksFromFile(String f) 
-        {
-            File file = new File(f);
-    
-            try 
-            {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                String st;
-    
-                int i=0;
-                int kol=0;
-                while((st = br.readLine()) != null) 
-                {
-                    String[] words = st.split("\\s+");
-                    for (int j=0; j<words.length; j++) 
-                    {
-                        double d = Double.parseDouble(words[j]);
-                        this.Mat[i][j] = d;
-                    }
-            
-                    kol = words.length;
-                    i++;
-                }
-                this.idxBaris = i;
-                this.idxKolom = kol;
-            }
-            catch (FileNotFoundException e) 
-            {
-                System.out.println(e);
-            }
-            catch (IOException e) 
-            {
-                System.out.println(e);
-            }
-        }
-        
-        public void TulisMatriks() 
-        {
-            for(int i=0; i<this.idxBaris; i++)
-            {
-                for(int j=0; j<this.idxKolom; j++)
-                {
-                    System.out.format("%.2f ",this.Mat[i][j]);
-                }
-                System.out.println();
-            }
-        }
-        
-        public void TulisMatriksToFile() 
-        {
-            String fileName = input.nextLine();
-            try 
-            {
-                FileWriter fw = new FileWriter(fileName);
-                for(int i=0; i<this.idxBaris; i++)
-                {
-                    for(int j=0; j<this.idxKolom; j++)
-                    {
-                        fw.write(Double.toString(this.Mat[i][j]));
-                        if (j!= this.idxKolom-1) 
-                        {
-                            fw.write(" ");
-                        }
-                    }
-                    if (i != this.idxBaris-1) 
-                    {
-                        fw.write(String.format("%n"));
-                    }
-                }
-                fw.close();
-            }
-            catch (Exception e) {
-                System.out.println(e);
-            }
-        }
->>>>>>> dba88f13f2a80ad0f68c23cbc178b8b7c776b3ac
     
         public void Interpolasi() 
         {
